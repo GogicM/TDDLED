491ff7139e61c96edec6a7ca63f995d83b713b79
Igor Kolak <igor.kolak@rt-rk.com>
2015-08-30 13:23:06 -0700

    Test 7 dodan radi lakseg drive-anja dizajna...

--- src/LedDriver.c
+++ src/LedDriver.c
@@ -28,3 +28,8 @@ void LedDriver_TurnOff(int ledNumber)
 	*ledsAddress = 0;
 }
 
+void LedDriver_TurnAllOn(void)
+{
+	*ledsAddress = 0xffff;
+}
+
--- tests/LedDriver_Test.c
+++ tests/LedDriver_Test.c
@@ -12,6 +12,7 @@ TEST_GROUP_RUNNER(LedDriver)
 	RUN_TEST_CASE(LedDriver, TurnOffLedOne);
 	RUN_TEST_CASE(LedDriver, TurnOnMultipleLeds);
 	RUN_TEST_CASE(LedDriver, TurnOffAnyLed);
+	RUN_TEST_CASE(LedDriver, AllOn);
 }
 
 TEST_SETUP(LedDriver)
@@ -22,6 +23,7 @@ TEST_SETUP(LedDriver)
 TEST_TEAR_DOWN(LedDriver)
 {
 }
+
 
 TEST(LedDriver, LedsOffAfterCreate)
 {
@@ -50,9 +52,16 @@ TEST(LedDriver, TurnOnMultipleLeds)
 	TEST_ASSERT_EQUAL_HEX16(0x180, virtualLeds);
 }
 
-TEST(LedDriver, TurnOffAnyLed)
+
+IGNORE_TEST(LedDriver, TurnOffAnyLed)
 {
 	LedDriver_TurnOn(9);
-	LedDriver_TurnOn(8); LedDriver_TurnOff(8); // upali pa ugasi osmu! ... ne zaboravi dodati u RUN :)
+	LedDriver_TurnOn(8); LedDriver_TurnOff(8); // stavi ga na ignore i dodaj drugi koji bi ovo bolje "primjetio"
 	TEST_ASSERT_EQUAL_HEX16(0x100, virtualLeds);
+}
+
+TEST(LedDriver, AllOn)
+{
+	LedDriver_TurnAllOn();
+	TEST_ASSERT_EQUAL_HEX16(0xffff, virtualLeds);
 }
