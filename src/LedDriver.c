45f6b685c1c4d091af8bc7829d341adc11d14b3d
Igor Kolak <igor.kolak@rt-rk.com>
2015-08-30 13:05:59 -0700

    Drugi test dodan - PASS

--- src/LedDriver.c
+++ src/LedDriver.c
@@ -1,8 +1,10 @@
 #include "LedDriver.h"
 
+static uint16_t * ledsAddress;
 void LedDriver_Create(uint16_t * address)
 {
-	*address = 0;
+	ledsAddress = address;
+	*ledsAddress = 0;
 }
 
 void LedDriver_Destroy(void)
@@ -11,4 +13,7 @@ void LedDriver_Destroy(void)
 
 void LedDriver_TurnOn(int ledNumber)
 {
+	*ledsAddress = 1;
 }
+
+
