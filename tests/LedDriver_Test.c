759c59d6b7f60ac223866639deed3bbd7119fa72
Igor Kolak <igor.kolak@rt-rk.com>
2015-08-30 13:11:56 -0700

    REFACTORING

--- tests/LedDriver_Test.c
+++ tests/LedDriver_Test.c
@@ -1,21 +1,24 @@
 #include "../unity/unity_fixture.h"
 #include "../src/LedDriver.h"
 
+static uint16_t virtualLeds;
+
 TEST_GROUP(LedDriver);
-
-TEST_SETUP(LedDriver)
-{
-}
-
-TEST_TEAR_DOWN(LedDriver)
-{
-}
 
 TEST_GROUP_RUNNER(LedDriver)
 {
 	RUN_TEST_CASE(LedDriver, LedsOffAfterCreate);
 	RUN_TEST_CASE(LedDriver, TurnOnLedOne);
 	RUN_TEST_CASE(LedDriver, TurnOffLedOne);
+}
+
+TEST_SETUP(LedDriver)
+{
+	LedDriver_Create(&virtualLeds);
+}
+
+TEST_TEAR_DOWN(LedDriver)
+{
 }
 
 TEST(LedDriver, LedsOffAfterCreate)
@@ -27,20 +30,14 @@ TEST(LedDriver, LedsOffAfterCreate)
 
 TEST(LedDriver, TurnOnLedOne)
 {
-	uint16_t virtualLeds;
-	LedDriver_Create(&virtualLeds);
 	LedDriver_TurnOn(1);
 	TEST_ASSERT_EQUAL_HEX16(1, virtualLeds);
 }
 
-// DODAO :)
 TEST(LedDriver, TurnOffLedOne)
 {
-	uint16_t virtualLeds;
-	LedDriver_Create(&virtualLeds);
 	LedDriver_TurnOn(1);
 	LedDriver_TurnOff(1);
 	TEST_ASSERT_EQUAL_HEX16(0, virtualLeds);
 }
-
 
