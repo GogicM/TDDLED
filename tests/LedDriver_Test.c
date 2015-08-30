e020952561f34644c795b787f8cbfddb75851793
Igor Kolak <igor.kolak@rt-rk.com>
2015-08-30 13:03:16 -0700

    Drugi test dodan - build failed

--- tests/LedDriver_Test.c
+++ tests/LedDriver_Test.c
@@ -14,6 +14,9 @@ TEST_TEAR_DOWN(LedDriver)
 TEST_GROUP_RUNNER(LedDriver)
 {
 	RUN_TEST_CASE(LedDriver, LedsOffAfterCreate);
+
+	// NE ZABORAVI OVO DODATI :D
+	RUN_TEST_CASE(LedDriver, TurnOnLedOne);
 }
 
 TEST(LedDriver, LedsOffAfterCreate)
@@ -21,5 +24,13 @@ TEST(LedDriver, LedsOffAfterCreate)
 	uint16_t virtualLeds = 0xffff;
 	LedDriver_Create(&virtualLeds);
 	TEST_ASSERT_EQUAL_HEX16(0, virtualLeds);
+}
+
+TEST(LedDriver, TurnOnLedOne)
+{
+	uint16_t virtualLeds;
+	LedDriver_Create(&virtualLeds);
+	LedDriver_TurnOn(1);
+	TEST_ASSERT_EQUAL_HEX16(1, virtualLeds);
 }
 
